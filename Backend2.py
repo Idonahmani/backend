0ca3417f9596498b37b3f955700c3594eeeea68b
Ido Nahmani <92860130+Idonahmani@users.noreply.github.com>
2021-12-20 14:48:55 +0200

    Add files via upload

--- Backend2.py
+++ Backend2.py
@@ -1,6 +1,9 @@
 from flask import Flask, redirect,url_for
 from flask import render_template
+from flask import request
+from flask import session
 app=Flask(__name__)
+app.secret_key = '12321'
 
 @app.route('/home_page')
 @app.route('/home')
@@ -15,20 +18,42 @@ def home_func():
 
 def hello_world():  # put application's code here
     # TODO
-    return redirect(url_for('about.html'))
+    return redirect(url_for('assignment8.html'))
 
-@app.route('/about')
-def about_func():
+@app.route('/assignment8')
+def assignment8_func():
     profile={'Name':'Ido','Last Name':'Nahmani'}
     uni='bgu'
     degrees=['BSc','Msc']
     hobbies=('art','sport','music','flask')
-    return render_template('about.html',fullname=profile,University=uni,degress=degrees,hobbies=hobbies)
+    return render_template('assignment8.html',fullname=profile,University=uni,degress=degrees,hobbies=hobbies)
 
 
 @app.route('/catalog')
 def catalog_func():
+    if 'product' in request.args:
+        product = request.args['product']
+        size = request.args['size']
+        return render_template('catalog.html',product=product,size=size)
     return render_template('catalog.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login_fun():
+    if request.method=='GET':
+        return render_template('login.html')
+    if request.method=='POST':
+        #DB
+        username = request.form['username']
+        password=request.form['password']
+        session['username']=username
+        session['password']=password
+        return redirect(url_for('home_func'))
+
+@app.route('/logout')
+def logout_func():
+    session['username']=''
+    return render_template('index.html')
+
 
 if __name__== '__main__':
     app.run(debug=True)
